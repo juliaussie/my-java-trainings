0eefdeb55ddaac9d1ec7ed38a5c1924adc6cba9b
Julia Khikhlovskaja <hihlovskajajulia@gmail.com>
2022-08-08 21:46:28 +0300

    date task

---
+++ src/com/company/tsks/imports/Main.java
@@ -0,0 +1,15 @@
+package com.company.tsks.imports;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        showCurrentDate();
+    }
+
+    public static void showCurrentDate()  {
+	// write your code here
+        System.out.println("npиBeT, сегодня: ");
+        System.out.println(new Date());
+    }
+}
